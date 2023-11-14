2c43414332f529bceedb5e22b1e623090524f715
hyominnnnn <dlalsgy23@gmail.com>
2023-11-14 17:48:08 +0900

    .

--- helloKoreaProject/src/main/java/com/kh/korea/board/controller/BoardController.java
+++ helloKoreaProject/src/main/java/com/kh/korea/board/controller/BoardController.java
@@ -1,4 +1,4 @@
-package com.kh.korea.borad;
+package com.kh.korea.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
