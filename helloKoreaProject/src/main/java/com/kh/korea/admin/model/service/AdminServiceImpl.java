03fa3788af9f6024dd287559937c3e461ac506c1
hyunzae <146050667+hyunzae@users.noreply.github.com>
2023-11-14 17:48:22 +0900

    ㅎㅎ

--- helloKoreaProject/src/main/java/com/kh/korea/admin/model/service/AdminServiceImpl.java
+++ helloKoreaProject/src/main/java/com/kh/korea/admin/model/service/AdminServiceImpl.java
@@ -1,8 +1,22 @@
 package com.kh.korea.admin.model.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kh.korea.admin.model.dao.AdminDao;
 
 @Service
 public class AdminServiceImpl implements  AdminService{
+
+	@Autowired
+	private AdminDao adminDao;
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Override
+	public int selectMemberList() {
+		return adminDao.selectMemberList(sqlSession);
+	}
 
 }
