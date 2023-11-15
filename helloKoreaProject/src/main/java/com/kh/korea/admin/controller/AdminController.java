616366d0fc1fdc3596b230df33df44ef62c77bf6
hyunzae <146050667+hyunzae@users.noreply.github.com>
2023-11-15 12:06:28 +0900

    gg

--- helloKoreaProject/src/main/java/com/kh/korea/admin/controller/AdminController.java
+++ helloKoreaProject/src/main/java/com/kh/korea/admin/controller/AdminController.java
@@ -1,9 +1,23 @@
 package com.kh.korea.admin.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
+	
+	@GetMapping("memberInfo")
+	public String memberInfo() {
+		return "admin/memberInfo";
+	}
+	
+	@RequestMapping("posting.me")
+	public String memberPosting(HttpServletRequest request) {
+		return "admin/memberPosting";
+	}
 	
 	
 
