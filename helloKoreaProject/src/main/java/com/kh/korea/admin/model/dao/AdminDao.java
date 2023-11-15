b29e2394d0413dafc59c1a187fbc541e80026d06
hyunzae <146050667+hyunzae@users.noreply.github.com>
2023-11-15 16:36:47 +0900

    ㅁㅁ

--- helloKoreaProject/src/main/java/com/kh/korea/admin/model/dao/AdminDao.java
+++ helloKoreaProject/src/main/java/com/kh/korea/admin/model/dao/AdminDao.java
@@ -1,8 +1,14 @@
 package com.kh.korea.admin.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class AdminDao {
+
+	public int selectMemberList(SqlSessionTemplate sqlSession) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
