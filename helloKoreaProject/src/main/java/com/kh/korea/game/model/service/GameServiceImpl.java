4890abd09cfdfc462a54a7108e7c6a8df550f2ab
rimriim <eerimi31@gmail.com>
2023-11-21 17:45:49 +0900

    게임

--- helloKoreaProject/src/main/java/com/kh/korea/game/model/service/GameServiceImpl.java
+++ helloKoreaProject/src/main/java/com/kh/korea/game/model/service/GameServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.korea.game.model.dao.GameDao;
+import com.kh.korea.game.model.vo.LowerGame;
 
 @Service
 public class GameServiceImpl implements GameService {
@@ -14,6 +15,21 @@ public class GameServiceImpl implements GameService {
 	
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+
+	@Override
+	public LowerGame firstQuiz() {
+		return gameDao.firstQuiz(sqlSession);
+	}
+
+	@Override
+	public LowerGame nextQuiz(int quizNo) {
+		return gameDao.nextQuiz(sqlSession, quizNo);
+	}
+
+	@Override
+	public LowerGame prevQuiz(int quizNo) {
+		return gameDao.prevQuiz(sqlSession, quizNo);
+	}
 	
 	
 
